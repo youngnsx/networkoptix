77d91ab355e17b2b1ff05c5a66468a1268cc2d7d
Youngnsx <youngnsx@gmail.com>
2022-11-28 13:41:59 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-"# networkoptix" 
+# NETWORKOPTIX on RPI
+- **version:**4.2.0.32840
+- **component:**server
+- **platform:**linux
+- **arch:**arm
+- **modification:**bpi
---
+++ install_docker.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+# Install docker
+curl -sSL https://get.docker.com | sh
+
+# Added User to Docekr Group
+sudo usermod -aG docker pi
+
+# Install Pip3
+# curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && sudo python3 get-pip.py
+
+### Install docker-compose
+sudo pip3 install docker-compose
+
+# Pull NX
+# git clone https://github.com/wafflecity/waffle_networkoptix -b raspbian
